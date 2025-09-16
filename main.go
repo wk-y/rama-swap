992de61d0587d3d7aca49c26d427fceb4c78bf5b
wk-y <86206045+wk-y@users.noreply.github.com>
2025-09-15 19:19:25 -0700

    Use custom argument parser
    This enables -ramalama to be parsed akin to `find -exec`.

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"log"
 	"net"
@@ -16,27 +15,50 @@ import (
 const defaultPort = 4917
 const defaultHost = "127.0.0.1"
 
-func main() {
-	host := flag.String("host", defaultHost, "ip address to listen on")
-	port := flag.Int("port", defaultPort, "port to listen on")
-	flag.Parse()
+const EX_USAGE = 64
 
-	l, err := net.Listen("tcp", fmt.Sprintf("%s:%d", *host, *port))
+func main() {
+	args, rest, err := parseArgs(os.Args)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s: %v\n", os.Args[0], err)
+		os.Exit(EX_USAGE)
+	}
+
+	if len(rest) > 0 {
+		fmt.Fprintf(os.Stderr, "%s: unexpected positional argument %v\n", os.Args[0], rest[0])
+		os.Exit(EX_USAGE)
+	}
+
+	// set default values for unspecified flags
+	if args.Host == nil {
+		host := "127.0.0.1"
+		args.Host = &host
+	}
+
+	if args.Port == nil {
+		port := 4917
+		args.Port = &port
+	}
+
+	if args.Ramalama == nil {
+		if env := os.Getenv("RAMALAMA_COMMAND"); env != "" {
+			args.Ramalama = strings.Split(env, " ")
+			if len(args.Ramalama) == 0 {
+				log.Fatalln("RAMALAMA_COMMAND environment variable should not be all whitespace")
+			}
+		} else {
+			args.Ramalama = []string{"ramalama"}
+		}
+	}
+
+	l, err := net.Listen("tcp", fmt.Sprintf("%s:%d", *args.Host, *args.Port))
 	if err != nil {
 		log.Fatalf("Failed to listen: %v", err)
 	}
 	defer l.Close()
 
-	ramalamaCommand := []string{"ramalama"}
-	if env := os.Getenv("RAMALAMA_COMMAND"); env != "" {
-		ramalamaCommand = strings.Split(env, " ")
-		if len(ramalamaCommand) == 0 {
-			log.Fatalln("RAMALAMA_COMMAND environment variable should not be all whitespace")
-		}
-	}
-
 	server := server.NewServer(ramalama.Ramalama{
-		Command: ramalamaCommand,
+		Command: args.Ramalama,
 	})
 
 	server.ModelNameMangler = func(s string) string {
@@ -45,7 +67,7 @@ func main() {
 
 	server.HandleHttp(http.DefaultServeMux)
 
-	log.Printf("Listening on http://%s:%d\n", *host, *port)
+	log.Printf("Listening on http://%s:%d\n", *args.Host, *args.Port)
 	err = http.Serve(l, nil)
 
 	log.Fatalf("Failed to serve: %v", err)
